e13019c311853516bdbb0e08ecb23432ab4a1974
Pillow's Tiny Desktop <109895040+ModulePillow@users.noreply.github.com>
2026-02-10 07:30:12 +0800

    Link to the correct CRT libs (/MT or /MTd)

--- SourceCode/ToolchainWin64.cmake
+++ SourceCode/ToolchainWin64.cmake
@@ -1,7 +1,9 @@
-   # Containing two platforms in single solution is not supported by CMake. e.g. x64+Win32
-   set(TARGET_WINDOWS_SDK 10.0.18362.0) #1903, 19H1
-   set(CMAKE_SYSTEM_VERSION ${TARGET_WINDOWS_SDK}) # Where CMake runs.
-   set(CMAKE_GENERATOR_PLATFORM x64,version=${TARGET_WINDOWS_SDK}) # Target machine.
-   add_compile_definitions(_UNICODE)
-   add_compile_options(/arch:SSE2 /Zc:__cplusplus) # Support SSE2, force the macro __cplusplus to return an updated value
-   set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded") # Equal to /MD, using static C/C++ runtime library
+# Containing two platforms in single solution is not supported by CMake. e.g. x64+Win32
+set(TARGET_WINDOWS_SDK 10.0.18362.0) #1903, 19H1
+set(CMAKE_SYSTEM_VERSION ${TARGET_WINDOWS_SDK}) # Where CMake runs.
+set(CMAKE_GENERATOR_PLATFORM x64,version=${TARGET_WINDOWS_SDK}) # Target machine.
+
+add_compile_definitions(_UNICODE)
+add_compile_options(/arch:SSE2 /Zc:__cplusplus) # Support SSE2, force the macro __cplusplus to return an updated value
+# Indicates /MT or /MTd (multithreaded, static link) to CRT (C/C++ runtime library).
+set(CMAKE_MSVC_RUNTIME_LIBRARY "$<IF:$<CONFIG:Debug>,MultiThreadedDebug,MultiThreaded>")
